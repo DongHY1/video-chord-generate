ebf20e304319cc9b80171ec23646243867e489b7
donghy1 <915110101@qq.com>
2022-04-17 16:39:05 +0800

    use gzip

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 export default defineConfig({
+    plugins: [viteCompression()],
    server:{
        headers:{
         "Cross-Origin-Opener-Policy": "same-origin",
